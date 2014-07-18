9deac10adf61a0021c08baecb6051c9e429fb440
Willian Justen <willianjustenqui@gmail.com>
2014-07-18 13:38:33 -0300

    Removing test files

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,2 @@
-var calc = require('./calc');
-
-console.log(calc.addTwoNumbers(5,5));
-console.log(calc.subTwoNumbers(5,5));
-console.log(calc.multTwoNumbers(5,5));
-console.log(calc.divideTwoNumbers(10,1));
+// put your code here
+// remember, you can use modules with browserify
